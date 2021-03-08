197ff7fd33a2280801abeb046717b5723b193315
Ermygut <ermygut@gmail.com>
2021-03-08 13:30:04 +0200

    recursion basis

--- js/script.js
+++ js/script.js
@@ -72,6 +72,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		</li>
 		`;
 		});
+
+		document.querySelectorAll('.delete').forEach((btn, i) => {
+			btn.addEventListener('click', () => {
+				btn.parentElement.remove();
+				movieDB.movies.splice(i, 1);
+
+				createMovieList(movieDB.movies, movieList);
+
+
+			});
+
+		});
 	}
 
 	createMovieList(movieDB.movies, movieList);
